5f2444c9c3d5dd0a7663a54b443bed3e1c09b27f
Luca <lucakanyo@protonmail.com>
2020-08-24 13:36:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(array, event){
+    let my_array = []
+    for (let i = 0; i < array.length; i++) {
+        my_array.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return my_array
+}
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number)
+        number--
+    }
+
+}
